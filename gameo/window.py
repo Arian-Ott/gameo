8b20e58bcdd2e6c9bd9ed302af6552f7b6b4caa0
Arian-Ott <88984301+Arian-Ott@users.noreply.github.com>
2025-07-02 16:10:27 +0200

    Add comment to clarify camera control disabling in GameoWindow
    Signed-off-by: Arian-Ott <88984301+Arian-Ott@users.noreply.github.com>

--- gameo/window.py
+++ gameo/window.py
@@ -6,6 +6,8 @@ from direct.showbase.ShowBase import ShowBase
 class GameoWindow(ShowBase):
     def __init__(self):
         super().__init__()
+        # disables the default camera control
+        self.disable_mouse()
         scene = self.loader.loadModel("models/box")
         scene.setPos(0, 10, 0)
 
